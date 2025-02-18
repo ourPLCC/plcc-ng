8b895749b5625e122377b40761c75fb84a42b4a9
KyleAlme <123590532+KyleAlme@users.noreply.github.com>
2025-02-18 07:07:26 +0000

    failing: create initial tests and mock Matcher

--- src/plccng/scanner/scanner.py
+++ src/plccng/scanner/scanner.py
@@ -1,11 +1,25 @@
-import json
 import pytest
 from .structs import Line, Match, Skip, Token, LexError
 
 class Scanner:
-    def __init__(self):
-        self.matcher = Matcher()
+    def __init__(self, matcher):
+        self.matcher = matcher
 
 
     def scan(self, lines):
-        return 0
+        if not lines or lines == []:
+            yield None
+        for line in lines:
+            index = 0 #Placeholder value
+            yield self.matcher.match(line, index)
+    
+
+#Temporary solution to "matching" tokens until scanner/matcher.py can be used directly
+class Matcher:
+    def __init__(self, spec):
+        self.spec = spec
+    
+    def match(self, line, index):
+        return None
+
+            
--- src/plccng/scanner/scanner_test.py
+++ src/plccng/scanner/scanner_test.py
@@ -1,1 +1,57 @@
 import pytest
+from .scanner import Scanner, Matcher
+from .structs import Line, Match, Token, Skip, LexError
+
+def test_no_lines_given_returns_nothing():
+    lines = None
+    scanner = make_scanner()
+    results = scanner.scan(lines)
+    assert next(results) == None
+
+def test_empty_lines_given_returns_nothing():
+    lines = []
+    scanner = make_scanner()
+    results = scanner.scan(lines)
+    assert next(results) == None    
+
+def test_one_token_matches():
+    lines = [Line("-", 1, None)]
+    scanner = make_scanner()
+    results = scanner.scan(lines)
+    assert next(results) == Token(lexeme="-", name="MINUS", line=Line("-", 1, None), column=0)  
+
+def test_multiple_tokens_match():
+    lines = [   Line("-", 1, None),
+                Line("+", 2, None),
+                Line("-", 3, None)]
+    scanner = make_scanner()
+    results = scanner.scan(lines)
+    assert next(results) == Token(lexeme="-", name="MINUS", line=Line("-", 1, None), column=0)  
+    assert next(resutls) == Token(lexeme="+", name="MINUS", line=Line("+", 2, None), column=0)
+    assert next(results) == Token(lexeme="-", name="MINUS", line=Line("-", 3, None), column=0)  
+
+def make_scanner():
+    matcher = make_matcher()
+    scanner = Scanner(matcher)
+    return scanner
+
+def make_matcher():
+    mock_speck = make_mock_spec
+    matcher = Matcher(mock_speck)
+    return matcher
+
+def make_mock_spec():
+    spec = [
+        {
+            "type": "Token",
+            "name": "MINUS",
+            "regex": "-"
+        },
+        {
+            "type": "Token",
+            "name": "ADDING",
+            "regex": "+"
+        }
+    ]
+
+
