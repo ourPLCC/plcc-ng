499559ed55a6625f918398d69546a709165fb658
Toby Trench <131901512+TheDragonApple@users.noreply.github.com>
2025-02-22 18:37:26 +0000

    Refactor tests so spec is unique to each test

--- src/plccng/scanner/matcher/matcher_test.py
+++ src/plccng/scanner/matcher/matcher_test.py
@@ -5,7 +5,13 @@ from plccng.scanner.matcher import matcher
 
 
 def test_empty_line():
-    matcher = make_matcher(spec=spec)
+    matcher = make_matcher(spec=[
+        {
+            "type": "Token",
+            "name": "MINUS",
+            "regex": "-"
+        }
+    ])
     line = Line(text="", file=None, number = 1)
     result = matcher.match(line = line, index = 0)
     assert result == LexError(line=line, column = 1)
@@ -17,50 +23,70 @@ def test_empty_spec():
     assert result == LexError(line=line, column=1)
 
 def test_match_rule():
-    matcher = make_matcher(spec = spec)
+    matcher = make_matcher(spec = [
+        {
+            "type": "Token",
+            "name": "MINUS",
+            "regex": "-"
+        }
+    ])
     line = Line(text="-", file=None, number=1)
     result = matcher.match(line=line, index=0)
     assert result == Token(lexeme="-", name="MINUS", column=1)
 
 def test_match_first_rule():
-    matcher = make_matcher(spec = spec)
-    line = Line(text="113--", file=None, number=1)
-    result = matcher.match(line=line, index=0)
-    assert result == Token(lexeme="113", name="NUMBER", column=3)
-
-def test_match_skip_rule():
-    matcher = make_matcher(spec = spec)
-    line = Line(text="   555", file=None, number=1)
-    result = matcher.match(line=line, index=0)
-    assert result == Skip(lexeme="   ", name="WHITESPACE", column=4)
-
-def test_match_longest_rule():
-    matcher = make_matcher(spec = spec)
-    line = Line(text="1235564", file=None, number=1)
-    result = matcher.match(line=line, index=0)
-    assert result == Token(lexeme="1235564", name="NUMBER", column=7)
-
-def test_match_first_longest_rule():
-    matcher = make_matcher(spec = spec)
-    line = Line(text="123", file=None, number=1)
-    result = matcher.match(line=line, index=0)
-    assert result == Token(lexeme="123", name = "ONETWOTHREE", column=3)
-
-
-
-#helper methods
-spec = [
+    matcher = make_matcher(spec = [
         {
             "type": "Token",
             "name": "MINUS",
             "regex": "-"
         },
+        {
+            "type": "Token",
+            "name": "NUMBER",
+            "regex": "\\d+"
+        }
+    ])
+    line = Line(text="113--", file=None, number=1)
+    result = matcher.match(line=line, index=0)
+    assert result == Token(lexeme="113", name="NUMBER", column=3)
 
+def test_match_skip_rule():
+    matcher = make_matcher(spec = [
+        {
+            "type": "Token",
+            "name": "NUMBER",
+            "regex": "\\d+"
+        },
         {
             "type": "Skip",
             "name": "WHITESPACE",
             "regex": "\\s+"
+        }
+    ])
+    line = Line(text="   555", file=None, number=1)
+    result = matcher.match(line=line, index=0)
+    assert result == Skip(lexeme="   ", name="WHITESPACE", column=4)
+
+def test_match_longest_rule():
+    matcher = make_matcher(spec = [
+        {
+            "type": "Token",
+            "name": "NUMBER",
+            "regex": "\\d+"
         },
+        {
+            "type": "Token",
+            "name": "ONETWOTHREE",
+            "regex": "123"
+        }
+    ])
+    line = Line(text="1235564", file=None, number=1)
+    result = matcher.match(line=line, index=0)
+    assert result == Token(lexeme="1235564", name="NUMBER", column=7)
+
+def test_match_first_longest_rule():
+    matcher = make_matcher(spec = [
         {
             "type": "Token",
             "name": "ONETWOTHREE",
@@ -71,7 +97,14 @@ spec = [
             "name": "NUMBER",
             "regex": "\\d+"
         }
-    ]
+    ])
+    line = Line(text="123", file=None, number=1)
+    result = matcher.match(line=line, index=0)
+    assert result == Token(lexeme="123", name = "ONETWOTHREE", column=3)
+
+
+
+#helper methods
 
 def make_matcher(spec):
     my_matcher = matcher.Matcher(spec)
