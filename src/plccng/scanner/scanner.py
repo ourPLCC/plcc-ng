e546185968d309cc87efd924418661146b1e2c19
KyleAlme <123590532+KyleAlme@users.noreply.github.com>
2025-02-23 06:52:40 +0000

    Create initial index finder

--- src/plccng/scanner/scanner.py
+++ src/plccng/scanner/scanner.py
@@ -5,14 +5,25 @@ class Scanner:
     def __init__(self, matcher):
         self.matcher = matcher
 
-
     def scan(self, lines):
         if not lines or lines == []:
             yield None
         for line in lines:
-            index = 0 #Placeholder value
-            yield self.matcher.match(line, index)
+            indexes = self._getLineIndexes(line)
+            for index in indexes:
+                yield self.matcher.match(line, index)
     
+    #Find the indexes of all the possible tokens and skips in a given line to feed to the matcher in "scan"
+    #FIX : Currently doesn't find index of whitespace
+    def _getLineIndexes(self, line):
+        indexes = []
+        line_text = line.text
+        possible_tokens = line_text.split()        
+        for token in possible_tokens:
+            index = line_text.index(token)
+            indexes.append(index)
+        return indexes
+
 
 #Temporary solution to "matching" tokens until scanner/matcher.py can be used directly
 class Matcher:
@@ -21,5 +32,3 @@ class Matcher:
     
     def match(self, line, index):
         return None
-
-            
--- src/plccng/scanner/scanner_test.py
+++ src/plccng/scanner/scanner_test.py
@@ -14,12 +14,20 @@ def test_empty_lines_given_returns_nothing():
     results = scanner.scan(lines)
     assert next(results) == None    
 
+def test_index_subroutine_finds_all_indexes():
+    line = Line("Plus Minus   PlusAGAIN", 1, None)
+    scanner = make_scanner()
+    results = scanner._getLineIndexes(line)
+    assert results == [0, 5, 13]
+
+@pytest.mark.skip(reason="This test is not yet covered. Where we are headed")
 def test_one_token_matches():
     lines = [Line("-", 1, None)]
     scanner = make_scanner()
     results = scanner.scan(lines)
     assert next(results) == Token(lexeme="-", name="MINUS", line=Line("-", 1, None), column=0)  
 
+@pytest.mark.skip(reason="This test is not yet covered. Where we are headed")
 def test_multiple_tokens_match():
     lines = [   Line("-", 1, None),
                 Line("+", 2, None),
