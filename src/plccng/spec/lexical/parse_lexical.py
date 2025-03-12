217cb050fe9fdccffd843fc50b52f3d4e85191d7
Stoney Jackson <dr.stoney@gmail.com>
2025-03-12 12:30:42 +0000

    refactor(spec.lexical): make single entry parse_lexical

---
+++ src/plccng/spec/lexical/parse_lexical.py
@@ -0,0 +1,10 @@
+from .parse_from_lines import parse_from_lines
+from .parse_from_string import parse_from_string
+
+
+def parse_lexical(thing, file=None, startLineNumber=1):
+    if isinstance(thing, str) or thing is None:
+        return parse_from_string(thing, file=file, startLineNumber=startLineNumber)
+    if isinstance(thing, list):
+        return parse_from_lines(thing)
+    raise TypeError(f'Unexpected type: {type(thing)}')
