43a12b06ef48de6919db887986939f07426a0a01
Stoney Jackson <dr.stoney@gmail.com>
2025-03-14 19:22:26 -0400

    refactor(spec.lines): single entry parseLines (#55)

--- src/plccng/spec/lines/__init__.py
+++ src/plccng/spec/lines/__init__.py
@@ -1,4 +1,2 @@
-from .parse_from_file import parse_from_file
-from .parse_from_string import parse_from_string
-from .parse_from_strings import parse_from_strings
+from .parseLines import parseLines
 from .Line import Line
--- src/plccng/spec/rough/parse_dividers_test.py
+++ src/plccng/spec/rough/parse_dividers_test.py
@@ -14,7 +14,7 @@ def test_empty_yields_nothing():
 
 
 def test_non_dividers_pass_through():
-    lines_ = list(parse_blocks(lines.parse_from_string('''\
+    lines_ = list(parse_blocks(lines.parseLines('''\
 one
 %%%
 two
@@ -25,49 +25,49 @@ three
 
 
 def test_one_divider():
-    lines_ = list(lines.parse_from_string('%'))
+    lines_ = list(lines.parseLines('%'))
     assert list(parse_dividers(lines_)) == [
         make_divider(tool='Java', language='Java', line=lines_[0])
     ]
 
 
 def test_one_divider_with_same_language_tool():
-    lines_ = list(lines.parse_from_string('% trailing'))
+    lines_ = list(lines.parseLines('% trailing'))
     assert list(parse_dividers(lines_)) == [
         make_divider(tool='trailing', language='trailing', line=lines_[0]),
     ]
 
 
 def test_one_divider_with_different_language_tool():
-    lines_ = list(lines.parse_from_string('% linter python '))
+    lines_ = list(lines.parseLines('% linter python '))
     assert list(parse_dividers(lines_)) == [
         make_divider(tool='linter', language='python', line=lines_[0]),
     ]
 
 
 def test_one_divider_with_different_but_same_language_tool():
-    lines_ = list(lines.parse_from_string('% python python'))
+    lines_ = list(lines.parseLines('% python python'))
     assert list(parse_dividers(lines_)) == [
         make_divider(tool='python', language='python', line=lines_[0]),
     ]
 
 
 def test_one_divider_only_takes_first_two_lines():
-    lines_ = list(lines.parse_from_string('% java python c++'))
+    lines_ = list(lines.parseLines('% java python c++'))
     assert list(parse_dividers(lines_)) == [
         make_divider(tool='java', language='python', line=lines_[0])
     ]
 
 
 def test_two_percents_does_not_match():
-    lines_ = list(lines.parse_from_string('%%'))
+    lines_ = list(lines.parseLines('%%'))
     assert list(parse_dividers(lines_)) == [
         lines.Line('%%', 1, None),
     ]
 
 
 def test_blocks_mask_dividers():
-    lines_ = list(parse_blocks(lines.parse_from_string('%%%\n%\n%%%')))
+    lines_ = list(parse_blocks(lines.parseLines('%%%\n%\n%%%')))
     assert list(parse_dividers(lines_)) == [
         Block([
             lines_[0].lines[0],
