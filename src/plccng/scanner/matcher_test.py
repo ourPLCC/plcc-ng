9537872bf03a02e796d2e1e1a1f2b0553f621684
Toby Trench <131901512+TheDragonApple@users.noreply.github.com>
2025-03-22 19:00:27 +0000

    Refactor structs in tests

--- src/plccng/scanner/matcher_test.py
+++ src/plccng/scanner/matcher_test.py
@@ -1,5 +1,5 @@
 from ..spec import parse_lexical_from_string
-from ..spec import Line
+from ..spec import Line, parse_lines_from_string
 from .Skip import Skip
 from .Token import Token
 from .LexError import LexError
@@ -8,73 +8,71 @@ from . import matcher
 
 
 def test_empty_line():
-    spec = parse_lexical_from_string(string = '''token MINUS '-' ''')
-    matcher = make_matcher(spec=spec[0].ruleList)
-    line = Line(string="", file=None, number = 1)
-    result = matcher.match(line = line, index = 0)
-    assert result == LexError(line=line, column = 1)
+    spec, errors = parse_lexical_from_string(string = '''token MINUS '-' ''')
+    matcher = make_matcher(spec=spec.ruleList)
+    result = matcher.match(line=Line(string="", number=0), index = 0)
+    assert result.__class__ == LexError
 
 def test_empty_spec():
     matcher = make_matcher(spec = [])
-    line = Line(string="This is a test", file=None, number=1)
+    line = list(parse_lines_from_string("This is a test."))[0]
     result = matcher.match(line = line, index = 0)
-    assert result == LexError(line=line, column=1)
+    assert result.__class__ == LexError
 
 def test_match_rule():
-    spec = parse_lexical_from_string(string='''token MINUS '-' ''')
-    print(spec)
-    matcher = make_matcher(spec=spec[0].ruleList)
-    line = Line(string="-", file=None, number=1)
+    spec, errors = parse_lexical_from_string(string='''token MINUS '-' ''')
+    matcher = make_matcher(spec=spec.ruleList)
+    line = list(parse_lines_from_string("-"))[0]
     result = matcher.match(line=line, index=0)
     assert result == Token(lexeme="-", name="MINUS", column=1)
 
 def test_match_first_rule():
-    spec = parse_lexical_from_string(string='''
+    spec, errors = parse_lexical_from_string(string='''
         token MINUS '-'
         token NUMBER '\\d+'
     ''')
-    matcher = make_matcher(spec=spec[0].ruleList)
-    line = Line(string="113--", file=None, number=1)
+    matcher = make_matcher(spec=spec.ruleList)
+    line = list(parse_lines_from_string("113--"))[0]
     result = matcher.match(line=line, index=0)
     assert result == Token(lexeme="113", name="NUMBER", column=3)
 
 def test_match_skip_rule():
-    spec = parse_lexical_from_string(string='''
+    spec, errors = parse_lexical_from_string(string='''
         token NUMBER '\\d+'
         skip WHITESPACE '\\s+'
     ''')
-    matcher = make_matcher(spec = spec[0].ruleList)
-    line = Line(string="   555", file=None, number=1)
+    matcher = make_matcher(spec = spec.ruleList)
+    line = list(parse_lines_from_string("   555"))[0]
     result = matcher.match(line=line, index=0)
     assert result == Skip(lexeme="   ", name="WHITESPACE", column=4)
 
 def test_match_longest_rule():
-    spec = parse_lexical_from_string(string='''
+    spec, errors = parse_lexical_from_string(string='''
         token NUMBER '\\d+'
         token ONETWOTHREE '123'
     ''')
-    matcher = make_matcher(spec=spec[0].ruleList)
-    line = Line(string="1235564", file=None, number=1)
+    matcher = make_matcher(spec=spec.ruleList)
+    line = list(parse_lines_from_string("1235564"))[0]
     result = matcher.match(line=line, index=0)
     assert result == Token(lexeme="1235564", name="NUMBER", column=7)
 
 def test_match_first_longest_rule():
-    spec = parse_lexical_from_string(string='''
+    spec, errors = parse_lexical_from_string(string='''
         token ONETWOTHREE '123'
         token NUMBER '\\d+'
     ''')
-    matcher = make_matcher(spec=spec[0].ruleList)
-    line = Line(string="123", file=None, number=1)
+    matcher = make_matcher(spec=spec.ruleList)
+    line = list(parse_lines_from_string("123"))[0]
     result = matcher.match(line=line, index=0)
     assert result == Token(lexeme="123", name = "ONETWOTHREE", column=3)
 
 def test_match_later_index():
-    spec = parse_lexical_from_string(string='''
+    spec, errors = parse_lexical_from_string(string='''
         token ONETWOTHREE '123'
         token NUMBER '\\d+'
     ''')
-    matcher = make_matcher(spec=spec[0].ruleList)
-    line = Line(string="123456789", file=None, number=1)
+    matcher = make_matcher(spec=spec.ruleList)
+    line = list(parse_lines_from_string("123456789"))[0]
     result = matcher.match(line=line, index=3)
     assert result == Token(lexeme="456789", name = "NUMBER", column=9)
 
