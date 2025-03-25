ba0120a4e9a11b7be2a584475c7dcebd128594f4
Stoney Jackson <dr.stoney@gmail.com>
2025-03-25 16:25:50 +0000

    WIP: clean tests

--- src/plccng/scanner/LexError.py
+++ src/plccng/scanner/LexError.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from ..spec import Line
+from ..lines import Line
 
 
 @dataclass
--- src/plccng/scanner/matcher_test.py
+++ src/plccng/scanner/matcher_test.py
@@ -1,94 +1,111 @@
-from ..spec import parse_lexical_from_string
-from ..spec import Line, parse_lines_from_string
+from ..lines import parseLines
+from ..spec import parseSpec
+from . import matcher
+from .LexError import LexError
 from .Skip import Skip
 from .Token import Token
-from .LexError import LexError
-
-from . import matcher
 
 
 def test_empty_line():
-    spec, errors = parse_lexical_from_string(string = '''token MINUS '-' ''')
-    matcher = make_matcher(spec=spec.ruleList)
-    result = matcher.match(line=Line(string="", number=0), index = 0)
+    matcher = makeMatcher(r'''token MINUS '-' ''')
+    emptyLine = parseLine('')
+    result = matcher.match(emptyLine, index = 0)
     assert result.__class__ == LexError
 
 def test_empty_spec():
-    matcher = make_matcher(spec = [])
-    line = list(parse_lines_from_string("This is a test."))[0]
-    result = matcher.match(line = line, index = 0)
+    emptySpec = []
+    matcher = makeMatcher(emptySpec)
+    line = parseLine("This is a test.")
+    result = matcher.match(line, index=0)
     assert result.__class__ == LexError
 
-def test_match_rule():
-    spec, errors = parse_lexical_from_string(string='''token MINUS '-' ''')
-    matcher = make_matcher(spec=spec.ruleList)
-    line = list(parse_lines_from_string("-"))[0]
-    result = matcher.match(line=line, index=0)
+def test_match_token():
+    matcher = makeMatcher(r'''token MINUS '-' ''')
+    line = parseLine("-")
+    result = matcher.match(line, index=0)
     assert result == Token(lexeme="-", name="MINUS", column=1)
 
-def test_match_first_rule_in_file():
-    spec, errors = parse_lexical_from_string(string='''
+def test_match_skip():
+    matcher = makeMatcher(r'''skip MINUS '-' ''')
+    line = parseLine("-")
+    result = matcher.match(line, index=0)
+    assert result == Skip(lexeme="-", name="MINUS", column=1)
+
+def test_match_token_with_multiple_rules():
+    matcher = makeMatcher(r'''
         token MINUS '-'
-        token NUMBER '\\d+'
+        token NUMBER '\d+'
     ''')
-    matcher = make_matcher(spec=spec.ruleList)
-    line = list(parse_lines_from_string("11--"))[0]
-    result = matcher.match(line=line, index=0)
+    line = parseLine("11--")
+    result = matcher.match(line, index=0)
     assert result == Token(lexeme="11", name="NUMBER", column=2)
 
-def test_match_skip_rule():
-    spec, errors = parse_lexical_from_string(string='''
-        skip WHITESPACE '\\s+'
-    ''')
-    matcher = make_matcher(spec = spec.ruleList)
-    line = list(parse_lines_from_string("   555"))[0]
-    result = matcher.match(line=line, index=0)
-    assert result == Skip(lexeme="   ", name="WHITESPACE", column=3)
-
 def test_match_longest_rule():
-    spec, errors = parse_lexical_from_string(string='''
-        token NUMBER '\\d+'
+    matcher = makeMatcher(r'''
+        token NUMBER '\d+'
         token ONETWOTHREE '123'
     ''')
-    matcher = make_matcher(spec=spec.ruleList)
-    line = list(parse_lines_from_string("1235564"))[0]
-    result = matcher.match(line=line, index=0)
+    line = parseLine("1235564")
+    result = matcher.match(line, index=0)
+    assert result == Token(lexeme="1235564", name="NUMBER", column=7)
+
+    # order should not matter
+    matcher = makeMatcher(r'''
+        token ONETWOTHREE '123'
+        token NUMBER '\d+'
+    ''')
+    line = parseLine("1235564")
+    result = matcher.match(line, index=0)
     assert result == Token(lexeme="1235564", name="NUMBER", column=7)
 
 def test_match_first_longest_rule():
-    spec, errors = parse_lexical_from_string(string='''
+    matcher = makeMatcher(r'''
         token ONETWOTHREE '123'
-        token NUMBER '\\d+'
+        token NUMBER '\d+'
     ''')
-    matcher = make_matcher(spec=spec.ruleList)
-    line = list(parse_lines_from_string("123"))[0]
-    result = matcher.match(line=line, index=0)
+    line = parseLine("123")
+    result = matcher.match(line, index=0)
     assert result == Token(lexeme="123", name = "ONETWOTHREE", column=3)
 
-def test_match_later_index():
-    spec, errors = parse_lexical_from_string(string='''
+    # order matters
+    matcher = makeMatcher(r'''
+        token NUMBER '\d+'
         token ONETWOTHREE '123'
-        token NUMBER '\\d+'
     ''')
-    matcher = make_matcher(spec=spec.ruleList)
-    line = list(parse_lines_from_string("123456789"))[0]
-    result = matcher.match(line=line, index=3)
-    assert result == Token(lexeme="456789", name = "NUMBER", column=9)
+    line = parseLine("123")
+    result = matcher.match(line, index=0)
+    assert result == Token(lexeme="123", name = "NUMBER", column=3)
 
-def test_skip_ignored():
-    spec, errors = parse_lexical_from_string(string='''
+def test_skip_wins_if_it_matches_before_any_token_rules():
+    matcher = makeMatcher(r'''
         skip ONE '1'
-        token NUMBER '\\d+'
+        token NUMBER '\d+'
     ''')
-    matcher = make_matcher(spec=spec.ruleList)
-    line = list(parse_lines_from_string("123"))[0]
-    result = matcher.match(line=line, index=0)
+    line = parseLine("123")
+    result = matcher.match(line, index=0)
     assert result == Skip(lexeme='1', name='ONE', column=1)
 
+    # Now token ONETWO matches before skip ONE, so skip is ignored.
+    # But then token NUMBER wins because it matches longest.
+    matcher = makeMatcher(r'''
+        token ONETWO '12'
+        skip ONE '1'
+        token NUMBER '\d+'
+    ''')
+    line = parseLine("123")
+    result = matcher.match(line, index=0)
+    assert result == Token(lexeme='123', name='NUMBER', column=1)
 
 
 #helper methods
 
-def make_matcher(spec):
-    my_matcher = matcher.Matcher(spec)
-    return my_matcher
+def makeMatcher(spec):
+    if isinstance(spec, str):
+        spec, errors = parseSpec(spec)
+        return matcher.Matcher(spec.lexical.ruleList)
+    else:
+        return matcher.Matcher(spec)
+
+
+def parseLine(string):
+    return list(parseLines(string + '\n'))[0]
