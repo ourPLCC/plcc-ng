fc2aea7e4ded54439c6ad3d775186286f3a28668
Connor Francis <cf620152@wne.edu>
2025-03-25 10:29:06 -0400

    refactor: adjust source_test

--- src/plccng/scanner/source_test.py
+++ src/plccng/scanner/source_test.py
@@ -4,152 +4,101 @@ from ..lines import Line
 
 from .source import Source
 
-
 def test_none_returns_empty():
     assert list(Source(None)) == []
-
 
 def test_empty_returns_empty():
     assert list(Source([])) == []
 
-
 def test_random_file_throws(fs):
     with pytest.raises(OSError):
-        s = Source(["whereami"])
+        s = Source(['whereami'])
         assert list(s) == []
 
-
 def test_one_line_reads(fs):
-    createFile(fs)
-    source = Source(["main"])
-    assert list(source) == [createLine()]
-
+    fs.create_file("./word", contents="<hello> ::= WORLD")
+    source = Source(["word"])
+    assert list(source) == [makeLine("<hello> ::= WORLD",1,"word")]
 
 def test_two_lines_read(fs):
-    createFile(
-        fs,
-        contents="""
-               <hello> ::= WORLD
-               <plcc> ::= NEXTGEN
-               """,
-    )
+    fs.create_file("./word", contents='''
+                   <hello> ::= WORLD
+                   <clang> ::= SEGFAULT
+                   ''')
 
-    source = Source(["main"])
-    assert list(source) == [
-        createLine(),
-        createLine(string="<plcc> ::= NEXTGEN", number=2),
-    ]
-
+    source = Source(["word"])
+    assert list(source) == [makeLine("<hello> ::= WORLD",1,"word"),
+                            makeLine("<clang> ::= SEGFAULT",2,"word")]
 
 def test_two_files_read(fs):
-    createFile(fs)
-    createFile(fs, name="./helper", contents="<plcc> ::= NEXTGEN")
+    fs.create_file("./hello", contents="<hello> ::= WORLD")
+    fs.create_file("./world", contents="<clang> ::= SEGFAULT")
 
-    source = Source(["main", "helper"])
-    assert list(source) == [
-        createLine(),
-        createLine(string="<plcc> ::= NEXTGEN", file="helper"),
-    ]
-
+    source = Source(["hello", "world"])
+    assert list(source) == [makeLine("<hello> ::= WORLD",1,"hello"),
+                            makeLine("<clang> ::= SEGFAULT",1,"world")]
 
 def test_stdin_works(monkeypatch):
-    createStdin(monkeypatch)
+    monkeypatch.setattr('sys.stdin', io.StringIO('<hello> ::= FROM STDIN'))
     source = Source(["-"])
-    assert list(source) == [createStdinLine()]
-
+    assert list(source) == [makeLine("<hello> ::= FROM STDIN",1,"-")]
 
 def test_stdin_works_nestled(monkeypatch, fs):
-    createFile(fs)
-    createStdin(monkeypatch)
-    createFile(fs, name="./helper", contents="<plcc> ::= NEXTGEN")
-
-    source = Source(["main", "-", "helper"])
-    assert list(source) == [
-        createLine(),
-        createStdinLine(),
-        createLine(string="<plcc> ::= NEXTGEN", file="helper"),
-    ]
-
+    monkeypatch.setattr('sys.stdin', io.StringIO('<hello> ::= FROM STDIN'))
+    fs.create_file("./hello", contents="<hello> ::= WORLD")
+    fs.create_file("./world", contents="<clang> ::= SEGFAULT")
+    source = Source(["hello","-","world"])
+    assert list(source) == [makeLine("<hello> ::= WORLD",1,"hello"),
+                            makeLine("<hello> ::= FROM STDIN",1,"-"),
+                            makeLine("<clang> ::= SEGFAULT",1,"world")]
 
 def test_stdin_works_first(monkeypatch, fs):
-    createStdin(monkeypatch)
-    createFile(fs)
-    createFile(fs, name="./helper", contents="<plcc> ::= NEXTGEN")
-
-    source = Source(["-", "main", "helper"])
-    assert list(source) == [
-        createStdinLine(),
-        createLine(),
-        createLine(string="<plcc> ::= NEXTGEN", file="helper"),
-    ]
-
+    monkeypatch.setattr('sys.stdin', io.StringIO('<hello> ::= FROM STDIN'))
+    fs.create_file("./hello", contents="<hello> ::= WORLD")
+    fs.create_file("./world", contents="<clang> ::= SEGFAULT")
+    source = Source(["-","hello","world"])
+    assert list(source) == [makeLine("<hello> ::= FROM STDIN",1,"-"),
+                            makeLine("<hello> ::= WORLD",1,"hello"),
+                            makeLine("<clang> ::= SEGFAULT",1,"world")]
 
 def test_stdin_works_last(monkeypatch, fs):
-    createFile(fs)
-    createFile(fs, name="./helper", contents="<plcc> ::= NEXTGEN")
-    createStdin(monkeypatch)
+    monkeypatch.setattr('sys.stdin', io.StringIO('<hello> ::= FROM STDIN'))
+    fs.create_file("./hello", contents="<hello> ::= WORLD")
+    fs.create_file("./world", contents="<clang> ::= SEGFAULT")
+    source = Source(["hello","world", "-"])
+    assert list(source) == [makeLine("<hello> ::= WORLD",1,"hello"),
+                            makeLine("<clang> ::= SEGFAULT",1,"world"),
+                            makeLine("<hello> ::= FROM STDIN",1,"-")]
 
-    source = Source(["main", "helper", "-"])
-    assert list(source) == [
-        createLine(),
-        createLine(string="<plcc> ::= NEXTGEN", file="helper"),
-        createStdinLine(),
-    ]
+def test_multi_line_stdin(monkeypatch, fs):
+    monkeypatch.setattr('sys.stdin', io.StringIO('''
+                                    <stda> ::= HELLO
+                                    <stdb> ::= FROM STDIN
+                                    '''))
+    fs.create_file("./hello", contents="<hello> ::= WORLD")
+    source = Source(["hello", "-"])
+    assert list(source) == [makeLine("<hello> ::= WORLD",1,"hello"),
+                            makeLine("<stda> ::= HELLO",1,"-"),
+                            makeLine("<stdb> ::= FROM STDIN",2,"-")]
 
-
-def test_multi_line_stdin(monkeypatch):
-    createStdin(
-        monkeypatch,
-        """
-                <stda> ::= HELLO
-                <stdb> ::= FROM STDIN
-                """,
-    )
-
-    source = Source(["-"])
-    assert list(source) == [
-        createStdinLine(string="<stda> ::= HELLO", number=1),
-        createStdinLine(string="<stdb> ::= FROM STDIN", number=2),
-    ]
-
-
-def test_stdin_escapes(monkeypatch):
-    createStdin(monkeypatch, "<stda> ::= HELLO\n<stdb> ::= FROM STDIN")
-
-    source = Source(["-"])
-    assert list(source) == [
-        createStdinLine(string="<stda> ::= HELLO", number=1),
-        createStdinLine(string="<stdb> ::= FROM STDIN", number=2),
-    ]
-
+def test_stdin_escapes(monkeypatch, fs):
+    monkeypatch.setattr('sys.stdin', io.StringIO('''<stda> ::= HELLO\n<stdb> ::= FROM STDIN'''))
+    fs.create_file("./hello", contents="<hello> ::= WORLD")
+    source = Source(["hello", "-"])
+    assert list(source) == [makeLine("<hello> ::= WORLD",1,"hello"),
+                            makeLine("<stda> ::= HELLO",1,"-"),
+                            makeLine("<stdb> ::= FROM STDIN",2,"-")]
 
 def test_next_iterates(monkeypatch, fs):
-    createFile(fs)
-    createStdin(
-        monkeypatch,
-        """
-                <stda> ::= HELLO
-                <stdb> ::= FROM STDIN
-                """,
-    )
+    monkeypatch.setattr('sys.stdin', io.StringIO('''
+                                    <stda> ::= HELLO
+                                    <stdb> ::= FROM STDIN
+                                    '''))
+    fs.create_file("./hello", contents="<hello> ::= WORLD")
+    source = Source(["hello", "-"])
+    assert next(source) ==  makeLine("<hello> ::= WORLD",1,"hello")
+    assert next(source) ==  makeLine("<stda> ::= HELLO",1,"-")
+    assert next(source) ==  makeLine("<stdb> ::= FROM STDIN",2,"-")
 
-    source = iter(Source(["main", "-"]))
-    assert next(source) == createLine()
-    assert next(source) == createStdinLine(string="<stda> ::= HELLO", number=1)
-    assert next(source) == createStdinLine(string="<stdb> ::= FROM STDIN", number=2)
-
-
-def createFile(fs, name="./main", contents="<hello> ::= WORLD"):
-    fs.create_file(name, contents=contents)
-
-
-def createStdin(monkeypatch, contents="<hello> ::= FROM STDIN"):
-    monkeypatch.setattr("sys.stdin", io.StringIO(contents))
-
-
-def createLine(string="<hello> ::= WORLD", number=1, file="main"):
-    return Line(string=string, number=number, file=file)
-
-
-def createStdinLine(string="<hello> ::= FROM STDIN", number=1):
-    return createLine(string=string, number=number, file="-")
+def makeLine(string, number, file=None):
+    return Line(string, number, file)
