9dcd229f61f3bfdb8e039206f011fa91e79d8415
michaelBanerjee <98488712+michaelBanerjee@users.noreply.github.com>
2025-03-27 17:12:27 +0000

    WIP throw HasLexErrors

--- src/plccng/scanner/main_test.py
+++ src/plccng/scanner/main_test.py
@@ -1,10 +1,7 @@
-"""
-Usage:
-    plccng scan --spec=<specfile> [<file> ...]
-"""
-
 from .main import Main
 from docopt import docopt
+import pytest
+from .structs import HasLexErrors
 
 def test_read_specfile_builds_matcher_spec(fs):
     createFile(fs, contents= '''
@@ -12,9 +9,10 @@ def test_read_specfile_builds_matcher_spec(fs):
         token MINUS '-'
     ''')
     string = "scan --spec=specfile"
-    args = docopt(__doc__, string)
+    args = docopt(getDocString(), string)
     main = Main(ScannerSpy, SourceMock, MatcherMock)
-    main.run(args)
+    with pytest.raises(SystemExit):
+        main.run(args)
     assert len(main.Scanner.matcher.spec) == 2
     assert main.Scanner.matcher.spec.ruleList[0].isSkip == True
     assert main.Scanner.matcher.spec.ruleList[1].name == "MINUS"
@@ -22,21 +20,55 @@ def test_read_specfile_builds_matcher_spec(fs):
 def test_read_input_file_and_pass_to_source(fs):
     createFile(fs)
     string = "scan --spec=specfile f1 - f2"
-    args = docopt(__doc__, string)
+    args = docopt(getDocString(), string)
     main = Main(ScannerSpy, SourceMock, MatcherMock)
-    main.run(args)
+    with pytest.raises(SystemExit):
+        main.run(args)
     assert main.Source.files == ['f1','-', 'f2']
 
-def test_scan_stdin_when_input_files_not_specified(fs):
+def test_Source_is_scanned(fs):
+    createFile(fs)
+    string = "scan --spec=specfile f1 - f2"
+    args = docopt(getDocString(), string)
+    main = Main(ScannerSpy, SourceMock, MatcherMock)
+    with pytest.raises(SystemExit):
+        main.run(args)
+    assert main.Scanner.scanned == main.Source.files
+
+def test_scan_stdin_symbol_when_input_files_not_specified(fs):
     createFile(fs)
     string = "scan --spec=specfile"
-    args = docopt(__doc__, string)
+    args = docopt(getDocString(), string)
     main = Main(ScannerSpy, SourceMock, MatcherMock)
-    main.run(args)
-    assert main.Source.files == ['-']
+    with pytest.raises(SystemExit):
+        main.run(args)
+    assert main.Scanner.scanned == ['-']
 
-def createFile(fs, name="./specfile", contents="token MINUS '-'"):
+def test_print_scan_results(fs, capfd):
+    createFile(fs)
+    string = "scan --spec=specfile f1 - f2"
+    args = docopt(getDocString(), string)
+    main = Main(ScannerSpy, SourceMock, MatcherMock)
+    with pytest.raises(SystemExit):
+        main.run(args)
+    captured = capfd.readouterr()
+    assert captured.out == f"Output of Scanner.scan({main.Scanner.scanned})\n"
+
+def test_raise_HasLexerros(fs):
+    createFile(fs, contents="this will raise LexError\n this too")
+    string = "scan --spec=specfile f1 - f2"
+    args = docopt(getDocString(), string)
+    main = Main(ScannerSpy, SourceMock, MatcherMock)
+
+    with pytest.raises(HasLexErrors) as exc_info:
+        main.run(args)
+    assert len(exc_info.value.lexErrors) == 2
+
+def createFile(fs, name="specfile", contents="token MINUS '-'"):
     fs.create_file(name, contents=contents)
+
+def getDocString():
+    return "Usage: plccng scan --spec=<specfile> [<file> ...]"
 
 class ScannerSpy:
     def __init__(self, matcher):
@@ -45,13 +77,11 @@ class ScannerSpy:
 
     def scan(self, lines):
         self.scanned = lines
+        yield f"Output of Scanner.scan({lines})"
 
 class MatcherMock:
     def __init__(self, spec):
         self.spec = spec
-
-    def match(string):
-        pass
 
 class SourceMock:
     def __init__(self, files):
@@ -59,6 +89,3 @@ class SourceMock:
 
     def __iter__(self):
         return iter(self.files)
-
-    def __next__(self):
-        return 'line'
