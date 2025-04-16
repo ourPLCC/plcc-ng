4a2a478d0e4ee89c6d7f1cac2117867b35dbf42e
michaelBanerjee <98488712+michaelBanerjee@users.noreply.github.com>
2025-04-16 19:24:40 +0000

    add filename

--- src/plccng/scanner/formatter.py
+++ src/plccng/scanner/formatter.py
@@ -1,6 +1,4 @@
-
 from .structs import Token
-from .Skip import Skip
 from .LexError import LexError
 from ..lines import Line
 
@@ -9,15 +7,12 @@ class Formatter:
         self.json = ''
 
     def format(self, tokensSkipsOrLexErrors):
-
         for obj in tokensSkipsOrLexErrors:
             yield self._formatAccordingToType(obj)
 
     def _formatAccordingToType(self, obj):
         if isinstance(obj, Token):
             return self._formatToken(obj)
-        elif isinstance(obj, Skip):
-            return self._formatSkip(obj)
         elif isinstance(obj, LexError):
             return self._formatLexError(obj)
 
@@ -27,18 +22,9 @@ class Formatter:
   "Type": "Token",
   "Name": "{token.name}",
   "Lexeme": "{token.lexeme}",
+  "File": "{token.line.file}",
   "Line": {token.line.number},
   "Column": {token.column}
-}}
-'''
-
-    def _formatSkip(self, skip):
-        return f'''
-{{
-  "Type": "Skip",
-  "Name": "{skip.name}",
-  "Lexeme": "{skip.lexeme}",
-  "Column": {skip.column}
 }}
 '''
 
@@ -46,6 +32,7 @@ class Formatter:
         return f'''
 {{
   "Type": "LexError",
+  "File": "{lexError.line.file}",
   "Line": {lexError.line.number},
   "Column": {lexError.column}
 }}
--- src/plccng/scanner/formatter_test.py
+++ src/plccng/scanner/formatter_test.py
@@ -1,52 +1,46 @@
 from .structs import Token
-from .Skip import Skip
 from .LexError import LexError
 from ..lines import Line
 from .formatter import Formatter
 import json
 
 
-# def test_empty():
-#     formatter = Formatter()
-#     jsonString = formatter.format([])
-#     assert next(jsonString) == []
+def test_empty():
+    formatter = Formatter()
+    strings = formatter.format(iter([]))
+    assert len(list(strings)) == 0
 
 def test_single_Token():
     formatter = Formatter()
-    strings = formatter.format([Token("lexeme", "name", makeLine("string", 1, "-"),column=3)])
+    strings = formatter.format(iter([Token("lexeme", "name", makeLine("string", 1, "fileName"),column=3)]))
     assert json.loads(next(strings)) == json.loads('''
 {"Type": "Token",
 "Name": "lexeme",
 "Lexeme": "name",
+"File": "fileName",
 "Line": 1,
-"Column": 3}''')
-
-def test_single_Skip():
-    formatter = Formatter()
-    strings = formatter.format([Skip("lexeme", "name", 3)])
-    assert json.loads(next(strings)) == json.loads('''
-{"Type": "Skip",
-"Name": "name",
-"Lexeme": "lexeme",
 "Column": 3}''')
 
 def test_single_lexError():
     formatter = Formatter()
-    strings = formatter.format([LexError(makeLine("string", 1, "-"), 3)])
+    strings = formatter.format(iter([LexError(makeLine("string", 1, "-"), 3)]))
     assert json.loads(next(strings)) == json.loads('''
 {"Type": "LexError",
+"File": "-",
 "Line": 1,
 "Column": 3}''')
 
 def test_consecutive():
     formatter = Formatter()
-    strings = formatter.format([LexError(makeLine("string", 1, "-"), 3), LexError(makeLine("string2", 2, "-"), 4)])
+    strings = formatter.format(iter([LexError(makeLine("string", 1, "fileName"), 3), LexError(makeLine("string2", 2, "-"), 4)]))
     assert json.loads(next(strings)) == json.loads('''
 {"Type": "LexError",
+"File": "fileName",
 "Line": 1,
 "Column": 3}''')
     assert json.loads(next(strings)) == json.loads('''
 {"Type": "LexError",
+"File": "-",
 "Line": 2,
 "Column": 4}''')
 
