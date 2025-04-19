ec193033ce59e7bdb35e83fddde64ddfcb6effde
Stoney Jackson <dr.stoney@gmail.com>
2025-04-19 22:11:42 +0000

    refactor: reformat tests

--- src/plccng/scanner/formatter_test.py
+++ src/plccng/scanner/formatter_test.py
@@ -1,28 +1,30 @@
-from .structs import Token
-from .LexError import LexError
-from ..lines import Line
-from .Skip import Skip
-from .formatter import Formatter
 import json
+
 import pytest
+
+from ..lines import Line
+from .formatter import Formatter
+from .LexError import LexError
+from .Skip import Skip
+from .structs import Token
 
 
 def test_empty():
     formatter = Formatter()
-    iterable = iter([])
-    strings = formatter.format(iterable)
+    strings = formatter.format([])
     assert len(list(strings)) == 0
+
 
 def test_invalid_type():
     formatter = Formatter()
     with pytest.raises(TypeError):
-        strings = formatter.format(iter(["invalid"]))
+        strings = formatter.format(["invalid"])
         next(strings)
+
 
 def test_single_Token():
     formatter = Formatter()
-    iterable = iter([makeToken()])
-    strings = formatter.format(iterable)
+    strings = formatter.format([makeToken()])
     assertNextJsonStringIsCorrect(strings, '''
         {
             "Type": "Token",
@@ -34,10 +36,10 @@ def test_single_Token():
         }
     ''')
 
+
 def test_single_lexError():
     formatter = Formatter()
-    iterable = iter([makeLexError()])
-    strings = formatter.format(iterable)
+    strings = formatter.format([makeLexError()])
     assertNextJsonStringIsCorrect(strings, '''
         {
             "Type": "LexError",
@@ -47,10 +49,10 @@ def test_single_lexError():
         }
     ''')
 
-def test_yield_skip_true():
+
+def test_yield_skips():
     formatter = Formatter(yieldSkips=True)
-    iterable = iter([makeSkip()])
-    strings = formatter.format(iterable)
+    strings = formatter.format([makeSkip()])
     assertNextJsonStringIsCorrect(strings, '''
         {
             "Type": "Skip",
@@ -62,16 +64,16 @@ def test_yield_skip_true():
         }
     ''')
 
-def test_yield_skip_false():
+
+def test_do_not_yield_skip():
     formatter = Formatter(yieldSkips=False)
-    iterable = iter([makeSkip()])
-    strings = formatter.format(iterable)
+    strings = formatter.format([makeSkip()])
     assert len(list(strings)) == 0
+
 
 def test_consecutive():
     formatter = Formatter()
-    iterable = iter([makeLexError(column=1), makeLexError(column=2)])
-    strings = formatter.format(iterable)
+    strings = formatter.format([makeLexError(column=1), makeLexError(column=2)])
     assertNextJsonStringIsCorrect(strings, '''
         {
             "Type": "LexError",
@@ -80,7 +82,6 @@ def test_consecutive():
             "Column": 1
         }
     ''')
-
     assertNextJsonStringIsCorrect(strings, '''
         {
             "Type": "LexError",
@@ -90,18 +91,23 @@ def test_consecutive():
         }
     ''')
 
+
 def assertNextJsonStringIsCorrect(strings, expected):
     jsonString = next(strings)
     assert json.loads(jsonString) == json.loads(expected)
 
+
 def makeLine(string="string", number=1, file="fileName"):
     return Line(string, number, file)
+
 
 def makeToken(name="name", lexeme="lexeme", line=makeLine(), column=2):
     return Token(name, lexeme, line, column)
 
+
 def makeSkip(lexeme="lexeme", name="name", line=makeLine(), column=2):
     return Skip(lexeme, name, line, column)
+
 
 def makeLexError(line=makeLine(), column=2):
     return LexError(line, column)
