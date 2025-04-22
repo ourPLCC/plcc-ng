b01024ccab348597987cc326bdca9f74f6dfe9ed
michaelBanerjee <98488712+michaelBanerjee@users.noreply.github.com>
2025-04-22 12:15:53 -0400

    feat: build src/plcc/scanner/sink.py (#61)
    Co-authored-by: Stoney Jackson <dr.stoney@gmail.com>

---
+++ src/plccng/scanner/sink.py
@@ -0,0 +1,12 @@
+from .formatter import format
+from .Skip import Skip
+
+
+class Sink:
+    def __init__(self, printSkips):
+        self.printSkips = printSkips
+
+    def write(self, obj):
+        if not isinstance(obj, Skip) or self.printSkips:
+            string = format(obj)
+            print(string)
