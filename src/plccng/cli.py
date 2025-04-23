9e0b26fa34848ab1cd47ab73239fbdc9242a7538
Stoney Jackson <dr.stoney@gmail.com>
2025-04-23 00:26:00 +0000

    WIP: junk

--- src/plccng/cli.py
+++ src/plccng/cli.py
@@ -1,10 +1,17 @@
-"""
+"""plccng
+
 Usage:
   plccng scan --spec=SPEC [FILE ...]
   plccng spec [FILE]
   plccng (-h | --help)
+"""
 
 from plccng.scanner import cli as scanner_cli
+import sys
+
+
+def main():
+    cli(sys.argv)
 
 
 def cli(argv):
@@ -22,19 +29,3 @@ class Cli:
             doc = sys.modules[Cli.__module__].__doc__
             print(doc)
 
-import sys
-
-from docopt import docopt
-
-from . import scanner
-from . import spec
-
-def main():
-    doc = sys.modules[main.__module__].__doc__
-    args = docopt(doc)
-    if args["scan"]:
-        scanner.run(args)
-    elif args['spec']:
-        spec.cli.run(sys.argv)
-    else:
-        print("Invalid Command, Enter 'plccng -h' for help")
