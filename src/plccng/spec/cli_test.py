297993c8ad97976688876dfbfcd1417ac50e85d4
Stoney Jackson <dr.stoney@gmail.com>
2025-04-24 14:20:11 +0000

    test(spec.cli): spec from stdin

--- src/plccng/spec/cli_test.py
+++ src/plccng/spec/cli_test.py
@@ -1,38 +1,65 @@
-import sys
+import io
+import json
 
 import docopt
 import pytest
-import json
 
 from .cli import cli
 
 
-def test_no_args_prints_usage(capsys, fs):
+def test_no_args_prints_usage():
     with pytest.raises(docopt.DocoptExit, match=r'Usage'):
         cli(['spec'])
 
 
-def test_h_prints_usage(capsys, fs):
+def test_h_prints_usage(capsys):
     with pytest.raises(SystemExit):
         cli('spec -h'.split())
-    out, err = capsys.readouterr()
-    assert 'Usage' in out
+    assertStdoutContains(capsys, 'Usage')
 
 
-def test_help_prints_usage(capsys, fs):
+def test_help_prints_usage(capsys):
     with pytest.raises(SystemExit):
         cli('spec --help'.split())
+    assertStdoutContains(capsys, 'Usage')
+
+
+def assertStdoutContains(capsys, string):
     out, err = capsys.readouterr()
-    assert 'Usage' in out
+    assert string in out
 
 
-def test_spec_from_file(capsys, fs):
-    fs.create_file("/spec.plcc", contents='''\
+def test_spec_from_file(capsys, fs, oneRuleSpec):
+    file = createFile(fs, oneRuleSpec)
+    cli(f'spec {file}'.split())
+    assertStdoutContainsJsonSpec(capsys, ruleCount=1)
+
+
+def test_spec_from_stdin(monkeypatch, capsys, oneRuleSpec):
+    setStdin(monkeypatch, oneRuleSpec)
+    cli('spec -'.split())
+    assertStdoutContainsJsonSpec(capsys, ruleCount=1)
+
+
+def createFile(fs, oneRuleSpec):
+    fs.create_file("/spec.plcc", contents=oneRuleSpec)
+    return "/spec.plcc"
+
+
+def setStdin(monkeypatch, string):
+    monkeypatch.setattr('sys.stdin', io.StringIO(string))
+
+
+def assertStdoutContainsJsonSpec(capsys, ruleCount):
+    out, err = capsys.readouterr()
+    result = json.loads(out)
+    assert len(result['lexical']['ruleList']) == ruleCount
+
+
+@pytest.fixture
+def oneRuleSpec():
+    return '''
 
             A 'a'
 
-    ''')
-    cli('spec /spec.plcc'.split())
-    out, err = capsys.readouterr()
-    result = json.loads(out)
-    assert len(result['lexical']['ruleList']) == 1
+    '''
