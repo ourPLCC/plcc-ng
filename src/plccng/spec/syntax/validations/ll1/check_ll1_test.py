e39c102dfe4970b7b81c01021b7518970b93c9e2
Stoney Jackson <dr.stoney@gmail.com>
2025-04-24 21:08:53 +0000

    test(ll1): left recursion not in ll1

--- src/plccng/spec/syntax/validations/ll1/check_ll1_test.py
+++ src/plccng/spec/syntax/validations/ll1/check_ll1_test.py
@@ -14,3 +14,17 @@ def test_left_factoring_ll1():
     g.addRule('a', ['(', ')'])
     errors = check_ll1(g)
     assert errors
+
+def test_direct_left_recursion_not_ll1():
+    g = Grammar()
+    g.addRule('a', ['a'])
+    errors = check_ll1(g)
+    assert errors
+
+
+def test_indirect_left_recursion_not_ll1():
+    g = Grammar()
+    g.addRule('a', ['b'])
+    g.addRule('b', ['a'])
+    errors = check_ll1(g)
+    assert errors
