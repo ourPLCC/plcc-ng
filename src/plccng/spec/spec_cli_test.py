0170ca7520b2a2b1a68fa074eb18d0ab861dbcc5
Stoney Jackson <dr.stoney@gmail.com>
2025-04-25 19:46:46 +0000

    feat(spec): make json output optional

--- src/plccng/spec/spec_cli_test.py
+++ src/plccng/spec/spec_cli_test.py
@@ -24,26 +24,34 @@ def test_help_prints_usage(capsys):
     assertStdoutContains(capsys, 'Usage')
 
 
-def assertStdoutContains(capsys, string):
-    out, err = capsys.readouterr()
-    assert string in out
-
-
 def test_spec_from_file(capsys, fs, oneRuleSpec):
     file = createFile(fs, oneRuleSpec)
     run(f'spec {file}'.split())
+    out, err = capsys.readouterr()
+    assert out == ''
+
+
+def test_spec_json_from_file_prints_json(capsys, fs, oneRuleSpec):
+    file = createFile(fs, oneRuleSpec)
+    run(f'spec --json {file}'.split())
     assertStdoutContainsJsonSpec(capsys, ruleCount=1)
 
 
 def test_spec_from_stdin(monkeypatch, capsys, oneRuleSpec):
     setStdin(monkeypatch, oneRuleSpec)
-    run('spec -'.split())
+    run('spec --json -'.split())
     assertStdoutContainsJsonSpec(capsys, ruleCount=1)
 
 
 def test_spec_with_one_error(capsys, fs, specWithNameExpectedError):
     file = createFile(fs, specWithNameExpectedError)
     run(f'spec {file}'.split())
+    assertStdoutContains(capsys, 'NameExpected')
+
+
+def test_spec_json_with_one_error(capsys, fs, specWithNameExpectedError):
+    file = createFile(fs, specWithNameExpectedError)
+    run(f'spec --json {file}'.split())
     assertStdoutContains(capsys, 'NameExpected')
 
 
@@ -56,11 +64,15 @@ def setStdin(monkeypatch, string):
     monkeypatch.setattr('sys.stdin', io.StringIO(string))
 
 
+def assertStdoutContains(capsys, string):
+    out, err = capsys.readouterr()
+    assert string in out
+
+
 def assertStdoutContainsJsonSpec(capsys, ruleCount):
     out, err = capsys.readouterr()
     result = json.loads(out)
     assert len(result['lexical']['ruleList']) == ruleCount
-
 
 
 @pytest.fixture
