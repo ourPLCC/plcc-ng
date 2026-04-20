98e04541d9b79edfa6b5ca34159be374760a1257
Stoney Jackson <dr.stoney@gmail.com>
2026-04-20 14:07:39 +0000

    chore(test): display versions of test dependencies

--- bin/install/bats.bash
+++ bin/install/bats.bash
@@ -5,7 +5,13 @@ BATS_VERSION="1.11.0"
 INSTALL_DIR="${HOME}/.local/lib/bats"
 BIN_DIR="${HOME}/.local/bin"
 
+versions() {
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    "${SCRIPT_DIR}/../versions.bash"
+}
+
 if command -v bats &>/dev/null && bats --version | grep -q "${BATS_VERSION}"; then
+    versions
     exit 0
 fi
 
@@ -16,3 +22,5 @@ trap 'rm -rf "${TMPDIR}"' EXIT
 git clone --depth 1 --branch "v${BATS_VERSION}" \
     https://github.com/bats-core/bats-core.git "${TMPDIR}/bats"
 "${TMPDIR}/bats/install.sh" "${HOME}/.local"
+
+versions
---
+++ bin/versions.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+pdm --version
+python --version
+python3 --version
+bats --version
