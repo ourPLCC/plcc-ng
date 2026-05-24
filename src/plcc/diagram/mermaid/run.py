4707895b837536b04791e62f41fde0689cdfa92e
Stoney Jackson <dr.stoney@gmail.com>
2026-05-24 18:08:59 +0000

    feat(diagram): add plcc-mermaid-diagram-run plugin

---
+++ src/plcc/diagram/mermaid/run.py
@@ -0,0 +1,45 @@
+import enum
+import platform
+import subprocess
+import sys
+
+from docopt import docopt
+
+from ...verbose import VerboseContext, VERBOSE_OPTIONS
+
+__doc__ = """plcc-mermaid-diagram-run
+    Open a diagram image in the system viewer.
+
+Usage:
+    plcc-mermaid-diagram-run --input=FILE [-v ...] [options]
+
+Options:
+    --input=FILE    Path to image file to open.
+    -h --help       Show this message.
+""" + VERBOSE_OPTIONS
+
+
+class Events(enum.Enum):
+    STARTED = "started"
+    FINISHED = "finished"
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    args = docopt(__doc__, argv)
+    verbose = VerboseContext.from_args("plcc-mermaid-diagram-run", Events, args)
+    input_file = args['--input']
+    verbose.emit(Events.STARTED, message=f"opening {input_file}")
+    _open_file(input_file)
+    verbose.emit(Events.FINISHED)
+
+
+def _open_file(path):
+    system = platform.system()
+    if system == 'Darwin':
+        subprocess.run(['open', path], check=True)
+    elif system == 'Windows':
+        subprocess.run(['start', path], shell=True, check=True)
+    else:
+        subprocess.run(['xdg-open', path], check=True)
