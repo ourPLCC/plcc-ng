fa9b89c688760463cb915d347d9609623df4b41c
Stoney Jackson <dr.stoney@gmail.com>
2026-05-26 13:07:36 +0000

    feat: add kind and hint properties to SpecError

--- src/plcc/spec/SpecError.py
+++ src/plcc/spec/SpecError.py
@@ -6,6 +6,14 @@ class SpecError(ValidationError):
         self.column = column if column is not None else index + 1
         ValidationError.__init__(self, line=line, message=message)
 
+    @property
+    def kind(self):
+        return self.message or type(self).__name__
+
+    @property
+    def hint(self):
+        return None
+
     def __str__(self):
         m = f'\n{self.message}' if self.message else ''
         return f'''{self.__class__.__name__}: {self.line.file}:{self.line.number}:{self.column}\n{self.line.string}\n{' '*(self.column-1)}^{m}\n'''
